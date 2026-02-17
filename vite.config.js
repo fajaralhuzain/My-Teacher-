b18e41420e214d8d9bd69065e01f89225cf3fae6
fajaralhuzain <fajaralhuzain@gmail.com>
2026-02-17 16:36:17 +0700

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/My-Teacher-/',
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
 })
